0725096a94d7439576fdb38af25842430cd89b25
Peter Thomas <ptrthomas@gmail.com>
2023-08-24 18:29:22 +0530

    no config required

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -1,12 +1,3 @@
 function fn() {
-  var KafkaUtils = Java.type('io.karatelabs.karate.kafka.KafkaUtils');
-  var config = {
-    'bootstrap.servers': '127.0.0.1:29092',
-   // 'value.serializer': 'io.confluent.kafka.serializers.KafkaAvroSerializer',
-    'schema.registry.url': 'http://localhost:8081'    
-  };  
-  var kafka = new KafkaUtils(config);
-  kafka.schema('hello', 'classpath:karate/hello.avsc');
-  kafka.schema('complex', 'classpath:karate/complex.avsc');
-  return { kafka: kafka };
+
 }
